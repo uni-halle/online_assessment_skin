24fab8f3d962f94a51ec5ad093aff5d3778515ca
Kristina-Haase <kristina.haase@itz.uni-halle.de>
2015-06-02 16:26:35 +0200

    online-assessment skin

---
+++ assessment/MathJax_config.js
@@ -0,0 +1,9 @@
+MathJax.Ajax.loadComplete("http://ilias.itz.uni-halle.de/Customizing/global/skin/mluilias/MathJax_config.js");
+
+MathJax.Hub.Config({
+    tex2jax: {
+        inlineMath: [ ['[tex]','[/tex]'] ],
+        processEscapes: true
+    }
+});
+
